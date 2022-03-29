c3776f4500f7b57b4f8b1ba1dfdf38003b5651e5
Cristhian Kelm <criskp007@gmail.com>
2022-03-29 10:50:21 -0400

    (UserController, read): create function read

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,7 +5,25 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+
 class UserController extends Controller
 {
-    //
+    private $loggedUser;
+
+    public function read()
+    {
+        $array = ['error' => ''];
+
+
+        $this->loggedUser = auth()->user();
+        $info = $this->loggedUser;
+        $info['avatar'] = url('media/avatars/' . $info['avatar']);
+        $array['data'] = $info;
+
+
+        return $array;
+    }
+
+
 }
